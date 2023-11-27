ed69e922615af152adaa7596582f14e267b29ff1
Iryna Stasheuski <istasheu@student.hive.fi>
2023-11-27 09:10:18 +0200

    fix: ft_put_unsigned norm error

--- ft_put_unsigned.c
+++ ft_put_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 12:40:22 by istasheu          #+#    #+#             */
-/*   Updated: 2023/11/26 16:42:18 by istasheu         ###   ########.fr       */
+/*   Updated: 2023/11/27 09:05:33 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	ft_put_unsigned(unsigned int n)
 {
-	int	check;
-	int	count;
+	int			check;
+	int			count;
 	const char	*base;
 
 	base = "0123456789";
 	count = 0;
 	if (n >= 10)
-		count  = ft_put_unsigned(n / 10);
+		count = ft_put_unsigned (n / 10);
 	if (count == -1)
 		return (-1);
 	check = write (1, &base[n % 10], 1);
