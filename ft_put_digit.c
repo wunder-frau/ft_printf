39f3d542ffcc199036504767d4e2432c2441be65
Iryna Stasheuski <istasheu@student.hive.fi>
2023-12-01 08:52:39 +0200

    test

--- ft_put_digit.c
+++ ft_put_digit.c
@@ -6,21 +6,19 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:10:09 by istasheu          #+#    #+#             */
-/*   Updated: 2023/11/27 08:47:58 by istasheu         ###   ########.fr       */
+/*   Updated: 2023/12/01 08:49:12 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_put_digit(int nb)
+int	ft_put_digit(int nb, const char *base)
 {
 	long		l_nb;
 	int			count;
-	const char	*base;
 	int			check;
 
 	l_nb = nb;
-	base = "0123456789";
 	count = 0;
 	if (l_nb < 0)
 	{
@@ -30,7 +28,7 @@ int	ft_put_digit(int nb)
 		l_nb = -1 * l_nb;
 	}
 	if (l_nb >= 10)
-		count += ft_put_digit(l_nb / 10);
+		count += ft_put_digit((l_nb / 10), base);
 	{
 		if (count == -1)
 			return (-1);
